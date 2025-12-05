594044c2d4cf90781f86990129e7d404db90f593
ytvee <yevgeniy.tyan.v@gmail.com>
2025-12-05 19:17:27 +0300

    clean

--- configs/strict.mjs
+++ configs/strict.mjs
@@ -1,10 +1,8 @@
-import { foldersRule } from './rules/folders.rule.mjs'
 import { oneModuleRule } from './rules/one-module.rule.mjs'
 import { structureRule } from './rules/structure.rule.mjs'
 import base from './base.mjs'
 
 export default [
-  foldersRule,
   ...base,
   {
     files: ['**/*.ts', '**/*.tsx'],
